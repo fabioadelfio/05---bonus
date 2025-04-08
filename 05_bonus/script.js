76a36d25ca0cca7a0f31ed4885ba688079c651fb
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-08 15:22:46 +0200

    bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -6,13 +6,28 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
-
+let outputText = ``;
 
 // Dichiara la funzione qui.
+const greetUser = (name) => {
+    const currentHour = new Date().getHours();
+    let greet = ``;
 
+    if(currentHour <= 13) {
+        greet = `Buongiorno`;
+    } else if(currentHour > 13 && currentHour <= 17) {
+        greet = `Buon pomeriggio`;
+    } else {
+        greet = `Buonasera`;
+    }
+
+    outputText = `${greet} ${name}`;
+
+    return outputText;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(greetUser(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
